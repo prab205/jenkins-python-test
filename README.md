c58bc8014a25862bb7133aa05db80066bd86fd2a
Prabin Paudel <prabinpaudel43@gmail.com>
2023-05-13 12:58:11 +0545

    Bugged flask file commited

---
+++ README.md
@@ -0,0 +1,21 @@
+# Object To Image (OTI)
+
+Converts paragraphs of text to its corresponding image and vice versa.
+
+Web application developed in Flask 2.2.3 <br />
+Hosted at [render](https://oti.onrender.com) <br />
+Might take ~30sec initially to get the service up and running.
+
+----
+**To run the application locally**
+1. Clone the github repo    
+`git clone https://github.com/prab205/Web-Object-Image.git`
+
+2. Enter the project directory    
+`cd Web-Object-Image`
+
+3. Install the dependencies    
+`pip install -r requirements.txt`
+
+4. Run the program    
+`flask --app app run`
--- run.py
+++ run.py
@@ -1,2 +1,28 @@
-print("This will be successfully executed")
+from Object2Image import ObjectAndImage
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+@app.route('/', methods =["GET", "POST"])
+def object2Image():
+   uploadedString = request.form.get("textInput")
+
+   if request.method == "POST" and uploadedString:
+      baseString = ObjectAndImage.objectToBase64(uploadedString)
+      return render_template("img.html", image_string = baseString)
+
+   return render_template("index.html")
+
+
+@app.route('/upload', methods =["GET", "POST"])
+def image2Object():
+   uploadedFile = request.files["inputImageName"]
+   
+   if request.method == "POST" and uploadedFile.filename != '':
+      user_obj = ObjectAndImage.imageToObjectWeb(uploadedFile.read())
+      return render_template("obj.html", user_output = user_obj )
+   
+   return render_template("index.html")
+
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=int("8000"), debug=True
